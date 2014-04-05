ffde895559974e3d6972a92d977dcb4ec627e68c
Erik Aybar <erikthedeveloper@gmail.com>
2014-04-05 13:44:29 -0600

    Initial commit. 2 scaffolds already created.

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :email, :username, :password, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoSimpleMvc::Application.config.secret_key_base = '1a71f699d0bb473ba8fc6f9a355914125ab7735fc30499595cf9bc19e511a8a225c99d3e87518e2b757c5092c606fae0f0813dfd1ad17cfc817cab2c454fa2b2'

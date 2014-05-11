1cd2b5b0a1eae4380a5edd832e8c700994cf00d8
Erik Aybar <erikthedeveloper@gmail.com>
2014-05-11 07:03:38 -0600

    Routes - removed root -> dashboard. Alternate syntax for many resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 TodoSimpleMvc::Application.routes.draw do
 
-  # root 'pages#home'
-  get "/" => "dashboard#index"
-
-  resources :tags
-  resources :tasks
-  resources :tasklists
-  resources :projects
-  resources :users
+  resources :tags,
+    :tasks,
+    :tasklists,
+    :projects,
+    :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
